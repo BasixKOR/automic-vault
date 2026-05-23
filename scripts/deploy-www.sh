ff9ce3941421a597d6a731e3da242467d2cf4466
Max Howell <mxcl@me.com>
2026-05-23 13:29:44 -0400

    fix pagefind usage

--- scripts/deploy-www.sh
+++ scripts/deploy-www.sh
@@ -568,7 +568,7 @@ ensure_response_headers_policy() {
         },
         ContentSecurityPolicy: {
           Override: true,
-          ContentSecurityPolicy: "default-src '\''self'\''; script-src '\''self'\'' '\''unsafe-inline'\''; style-src '\''self'\'' '\''unsafe-inline'\'' https://fonts.googleapis.com; font-src '\''self'\'' https://fonts.gstatic.com; img-src '\''self'\'' data: https://www.automicvault.com; connect-src '\''self'\''; frame-ancestors '\''none'\''; base-uri '\''self'\''; form-action '\''none'\''"
+          ContentSecurityPolicy: "default-src '\''self'\''; script-src '\''self'\'' '\''unsafe-inline'\'' '\''wasm-unsafe-eval'\''; style-src '\''self'\'' '\''unsafe-inline'\'' https://fonts.googleapis.com; font-src '\''self'\'' https://fonts.gstatic.com; img-src '\''self'\'' data: https://www.automicvault.com; connect-src '\''self'\''; frame-ancestors '\''none'\''; base-uri '\''self'\''; form-action '\''none'\''"
         }
       },
       CustomHeadersConfig: {
