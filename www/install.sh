34fd2be8df6c81a701016ad0bdfc94f55b091950
Max Howell <mxcl@me.com>
2026-06-03 07:46:35 -0400

    Reduce installer trace output

--- www/install.sh
+++ www/install.sh
@@ -2,18 +2,16 @@
 
 set -e
 
-expected_team_id="ZU76A67LGU"
-
 tmp="$(mktemp -d)"
+app="$tmp/mnt/Automic Vault.app"
 
 cleanup() {
+  set +x
   /usr/bin/hdiutil detach "$tmp/mnt" -quiet 2>/dev/null || true
   /bin/rm -rf "$tmp"
 }
 
 trap cleanup EXIT
-
-set -x
 
 /usr/bin/curl -sSfL https://automicvault.com/av.dmg -o "$tmp/av.dmg"
 
@@ -23,24 +21,15 @@ set -x
   -readonly \
   -quiet
 
-app="$(/usr/bin/find "$tmp/mnt" -maxdepth 1 -name '*.app' -print -quit)"
-
-[ -n "$app" ]
-
 /usr/sbin/spctl -a -vv --type exec "$app"
 
 /usr/bin/codesign --verify --deep --strict "$app"
 
 /usr/bin/codesign -dv --verbose=4 "$app" 2>&1 \
-  | /usr/bin/grep -q "^TeamIdentifier=${expected_team_id}$"
+  | /usr/bin/grep -q '^TeamIdentifier=ZU76A67LGU$'
 
-installed_app="/Applications/$(/usr/bin/basename "$app")"
-
-/usr/bin/ditto "$app" "$installed_app"
-
-av="$installed_app/Contents/Resources/av"
-
-[ -x "$av" ]
-
+/usr/bin/ditto "$app" "/Applications/Automic Vault.app"
 /usr/bin/sudo /bin/mkdir -p /usr/local/bin
-/usr/bin/sudo /usr/bin/install -m 755 "$av" /usr/local/bin/av
+/usr/bin/sudo /usr/bin/install -m 755 \
+  "/Applications/Automic Vault.app/Contents/Resources/av" \
+  /usr/local/bin/av
