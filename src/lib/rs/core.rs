a0ab7805b8742f138fabf25af43b4b7b9a6c42db
Max Howell <mxcl@me.com>
2026-06-18 11:50:11 -0400

    v1.24.0

--- src/lib/rs/core.rs
+++ src/lib/rs/core.rs
@@ -1,7 +1,7 @@
 use super::*;
 use std::collections::BTreeMap;
 
-pub(crate) const PROTOCOL_VERSION: &str = "1.17";
+pub(crate) const PROTOCOL_VERSION: &str = "1.18";
 
 fn is_false(value: &bool) -> bool {
     !*value
