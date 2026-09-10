eb5bb4ef656d006d5687c818978d637f7d153511
Max Howell <mxcl@me.com>
2026-09-10 18:37:11 -0400

    Release 4.8.2

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -61,7 +61,7 @@ modes:
 more:
   $ open https://www.automicvault.com/docs/";
 
-pub(crate) const INSTALL_REVISION: u32 = 51;
+pub(crate) const INSTALL_REVISION: u32 = 52;
 
 pub(crate) fn bash_shell_secret_insecurity_reasons() -> Result<Vec<String>, String> {
     shell_secrets::bash_reasons()
